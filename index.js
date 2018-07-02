96b096a78172317d5ced2666dd01ad3d52ff3bd6
Saima Parveen <saimaparveen05@gmail.com>
2018-07-02 19:53:44 +0530

    Delete index.js
    Wrong commit

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var Chart = require("chart.js");
-Chart = typeof Chart === "function" ? Chart : window.Chart;
-
-require("./chart.common.js")(Chart);
-require("./chart.errorBar.js")(Chart);
-require("./chart.horizontalErrorBar.js")(Chart);
