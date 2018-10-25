999fd51f9eff460a8527d07c77cce7022ecb1a56
Tanner Welsh <tannerwelsh@gmail.com>
2018-10-24 17:00:31 -0700

    refactor: Consolidate duplicate code into single file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ var Chart = require("chart.js");
 Chart = typeof Chart === "function" ? Chart : window.Chart;
 
 require("./chart.common.js")(Chart);
-require("./chart.errorBar.js")(Chart);
-require("./chart.horizontalErrorBar.js")(Chart);
 require("./chart.barPlus.js")(Chart);
