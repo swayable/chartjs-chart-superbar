243ea025ed166040ff26b16f1aaf2a9adc0c4460
Tanner Welsh <tannerwelsh@gmail.com>
2018-10-26 16:10:56 -0400

    feat: Add spacing between the bars
    refactor: Remove redundant isHorizontal property

--- src/mixin.barThickness.js
+++ src/mixin.barThickness.js
@@ -1,26 +1,12 @@
-export const calcBarThickness = (datum, options, frameSize) => {
-  let thickness = datum.thickness * frameSize
-
-  // TODO: use options for min/max value
-  // if (thickness < options.barThickness.min)
-  //   thickness = options.barThickness.min
-  // if (thickness > options.barThickness.max)
-  //   thickness = options.barThickness.max
-
-  return thickness
-}
-
 const BarThicknessMixin = {
   changeBarThickness() {
     const meta = this.getMeta(),
       data = this.getDataset().data,
       indexScale = this.getIndexScale(),
-      dimension = this.isHorizontal ? 'height' : 'width',
-      { options } = this.chart
+      dimension = indexScale.isHorizontal() ? 'width' : 'height'
 
     data.forEach((datum, index) => {
-      const frameSize = indexScale[dimension],
-        thickness = calcBarThickness(datum, options, frameSize)
+      const thickness = indexScale.getBarThickness(datum)
 
       meta.data[index]._view[dimension] = thickness
       meta.data[index]._model[dimension] = thickness
