9329db786338756a62a0fd5f080b3d4ff1035a2b
Tanner Welsh <tannerwelsh@gmail.com>
2018-11-26 15:23:37 -0800

    test: Sanity-check tests for chart

--- test/chart.barPlus.spec.js
+++ test/chart.barPlus.spec.js
@@ -7,110 +7,106 @@ import emptyDataset from './fixtures/emptyDataset'
 // Adapated from acquireChart()
 // 	https://github.com/chartjs/Chart.js/blob/master/test/utils.js
 const buildCanvas = (id = 'testChart') => {
-	const wrapper = document.createElement('div')
-	const canvas = document.createElement('canvas')
+  const wrapper = document.createElement('div')
+  const canvas = document.createElement('canvas')
 
-	canvas.id = id
-	canvas.width = 800
-	canvas.height = 600
+  canvas.id = id
+  canvas.width = 800
+  canvas.height = 600
 
-	wrapper.appendChild(canvas)
-	window.document.body.appendChild(wrapper)
+  wrapper.appendChild(canvas)
+  window.document.body.appendChild(wrapper)
 
-	return canvas
+  return canvas
 }
 
 const clearDOM = () => {
-	window.document.body.innerHTML = ''
+  window.document.body.innerHTML = ''
 }
 
 describe('chart.barPlus', () => {
-	let canvas, ctx, makeEmptyChart, makeChart
+  let canvas, ctx, makeEmptyChart, makeChart
 
-	beforeEach(() => {
-		canvas = buildCanvas()
-		ctx = canvas.getContext('2d')
+  beforeEach(() => {
+    canvas = buildCanvas()
+    ctx = canvas.getContext('2d')
 
-		makeEmptyChart = () => new Chart(ctx, {
-			type: 'barPlus',
-			data: {
-				datasets: [emptyDataset],
-				labels: [],
-			},
-		})
+    makeEmptyChart = () =>
+      new Chart(ctx, {
+        type: 'barPlus',
+        data: {
+          datasets: [emptyDataset],
+          labels: [],
+        },
+      })
 
-		makeChart = () => new Chart(ctx, {
-			type: 'barPlus',
-			data: {
-				datasets: [verticalDataset],
-				labels: ['A', 'B', 'C', 'D'],
-			},
-		})
-	})
+    makeChart = () =>
+      new Chart(ctx, {
+        type: 'barPlus',
+        data: {
+          datasets: [verticalDataset],
+          labels: ['A', 'B', 'C', 'D'],
+        },
+      })
+  })
 
-	afterEach(clearDOM)
+  afterEach(clearDOM)
 
-	describe('with an empty dataset and no options', () => {
-		it('should be constructed', () => {
-			const chart = makeEmptyChart()
+  describe('with an empty dataset and no options', () => {
+    it('should be constructed', () => {
+      const chart = makeEmptyChart()
 
-			expect(chart).not.toBe(undefined)
+      expect(chart).not.toBe(undefined)
 
-			const meta = chart.getDatasetMeta(0)
-			expect(meta.type).toEqual('barPlus')
-			expect(meta.data).toEqual([])
-			expect(meta.hidden).toBe(null)
-			expect(meta.controller).not.toBe(undefined)
-			expect(meta.controller.index).toBe(0)
-			expect(meta.xAxisID).not.toBe(null)
-			expect(meta.yAxisID).not.toBe(null)
-		})
-	})
+      const meta = chart.getDatasetMeta(0)
+      expect(meta.type).toEqual('barPlus')
+      expect(meta.data).toEqual([])
+      expect(meta.hidden).toBe(null)
+      expect(meta.controller).not.toBe(undefined)
+      expect(meta.controller.index).toBe(0)
+      expect(meta.xAxisID).not.toBe(null)
+      expect(meta.yAxisID).not.toBe(null)
+    })
+  })
 
-	describe('by default', () => {
-		it('uses the custom scales provided', () => {
-			const chart = makeEmptyChart()
+  describe('by default', () => {
+    it('uses the custom scales provided', () => {
+      const chart = makeEmptyChart()
 
-			const [xScale, yScale] = Object.values(chart.scales)
-			expect(xScale.type).toBe('categoryPlus')
-			expect(yScale.type).toBe('linearWithError')
-		})
+      const [xScale, yScale] = Object.values(chart.scales)
+      expect(xScale.type).toBe('categoryPlus')
+      expect(yScale.type).toBe('linearWithError')
+    })
 
-		it('shows and animates error bars', () => {
-			const chart = makeEmptyChart()
+    it('shows and animates error bars', () => {
+      const chart = makeEmptyChart()
 
-			const { errorBars } = chart.options
-			expect(errorBars.show).toBe(true)
-			expect(errorBars.animate).toBe(true)
-		})
+      const { errorBars } = chart.options
+      expect(errorBars.show).toBe(true)
+      expect(errorBars.animate).toBe(true)
+    })
+  })
 
-		it('uses a min/max bar thickness of 10px and 100px', () => {
-			const chart = makeEmptyChart()
+  describe('with non-empty dataset', () => {
+    it('creates the right element types', () => {
+      const chart = makeChart()
 
-			const { barThickness } = chart.options
-			expect(barThickness.min).toBe(10)
-			expect(barThickness.max).toBe(100)
-		})
-	})
+      const meta = chart.getDatasetMeta(0)
+      expect(meta.data.length).toBe(4)
+      expect(meta.data[0]).toBeInstanceOf(Chart.elements.Rectangle)
+      expect(meta.data[1]).toBeInstanceOf(Chart.elements.Rectangle)
+      expect(meta.data[2]).toBeInstanceOf(Chart.elements.Rectangle)
+      expect(meta.data[3]).toBeInstanceOf(Chart.elements.Rectangle)
+    })
 
-	describe('with non-empty dataset', () => {
-		it('creates the right element types', () => {
-			const chart = makeChart()
+    it('changes bg color when data item is insignificant', () => {
+      const chart = makeChart()
 
-			const meta = chart.getDatasetMeta(0)
-			expect(meta.data.length).toBe(4)
-			expect(meta.data[0]).toBeInstanceOf(Chart.elements.Rectangle)
-			expect(meta.data[1]).toBeInstanceOf(Chart.elements.Rectangle)
-			expect(meta.data[2]).toBeInstanceOf(Chart.elements.Rectangle)
-			expect(meta.data[3]).toBeInstanceOf(Chart.elements.Rectangle)
-		})
-
-		it('changes bg color when data item is insignificant', () => {
-			const chart = makeChart()
-
-			const meta = chart.getDatasetMeta(0)
-			expect(meta.data[0]._model.backgroundColor).toBe('#ff0000')
-			expect(meta.data[1]._model.backgroundColor).toBe('rgba(200, 200, 200, 0.8)')
-		})
-	})
+      const meta = chart.getDatasetMeta(0)
+      expect(meta.data[0]._model.backgroundColor).toBe('#ff0000')
+      expect(meta.data[1]._model.backgroundColor).toBe(
+        'rgba(200, 200, 200, 0.8)'
+      )
+    })
+  })
 })
